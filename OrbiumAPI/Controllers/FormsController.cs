652bd85964186816c0d0c8d7ad16f25f536f757d
Caio <ca.ragazzi@gmail.com>
2020-03-11 22:24:45 -0300

    first commit

---
+++ OrbiumAPI/Controllers/FormsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using OrbiumAPI.Model;
+
+namespace OrbiumAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FormsController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public FormsController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<Forms> Get()
+        {
+            using (SqlConnection conexao = new SqlConnection(_config.GetConnectionString("ExemplosDapper")))
+            {
+                return conexao.Query<Forms>("SELECT * FROM orb_forms");
+            }
+        }
+    }
+}
---
+++ OrbiumAPI/Model/Forms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrbiumAPI.Model
+{
+    public class Forms
+    {
+        public int frm_Form { get; set; }
+        public int frm_Operation { get; set; }
+        public string frm_Name { get; set; }
+        public string frm_Notes { get; set; }
+        public bool frm_Enabled { get; set; }
+        public DateTime frm_CreatedOn { get; set; }
+        public int frm_CreatedBy { get; set; }
+        public DateTime frm_ModifiedOn { get; set; }
+        public int frm_ModifiedBy { get; set; }
+    }
+}
